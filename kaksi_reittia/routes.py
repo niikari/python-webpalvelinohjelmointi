240562ac4f22f1950b2c0459d00f933359943478
Niiles <pakniiles@gmail.com>
2021-05-25 06:27:49 +0000

    tuntitehtävä

--- kaksi_reittia/routes.py
+++ kaksi_reittia/routes.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route("/home")
 def home():
-    return render_template(index.html)
+    return render_template("index.html")
 
 if __name__=="__main__":
     app.run()
+
+
+
+    
